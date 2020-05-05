53ca3a262f759eda382e7ae3eb30b528b530a38e
RTa_technology <57354947+RTa-technology@users.noreply.github.com>
2020-05-06 07:45:14 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,14 +16,24 @@ async def on_ready():
     channel = bot.get_channel(CHANNEL_ID)  
     await channel.send("Nervちゃん参上!")
 
-    
-    
-    
-    
+
 @bot.command(name="w")
 async def d(ctx, tenki: str):
     """!d {n}d{n}の書式で入力(合計表示のみ)"""
-    resp = urllib.request.urlopen('http://weather.livedoor.com/forecast/webservice/json/v1?city=%s'%tenki).read()
+    if tenki == "仙台":
+        resp = urllib.request.urlopen('http://weather.livedoor.com/forecast/webservice/json/v1?city=040010').read()
+    elif tenki =="東京":
+        resp = urllib.request.urlopen('http://weather.livedoor.com/forecast/webservice/json/v1?city=130010').read()
+    elif tenki =="横浜":
+        resp = urllib.request.urlopen('http://weather.livedoor.com/forecast/webservice/json/v1?city=140010').read()
+    elif tenki =="名古屋":
+        resp = urllib.request.urlopen('http://weather.livedoor.com/forecast/webservice/json/v1?city=230010').read()
+    elif tenki =="大阪":
+        resp = urllib.request.urlopen('http://weather.livedoor.com/forecast/webservice/json/v1?city=270000').read()
+    elif tenki =="岡山":
+        resp = urllib.request.urlopen('http://weather.livedoor.com/forecast/webservice/json/v1?city=340010').read()
+    elif tenki =="広島":
+        resp = urllib.request.urlopen('http://weather.livedoor.com/forecast/webservice/json/v1?city=340010').read()    
     resp = json.loads(resp.decode('utf-8'))
     msg = resp['location']['city']
     msg += "の天気は、\n"
