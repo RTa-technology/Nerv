360a7bd0cb411e838581018fb42b43e9fc0d6a21
RTa_technology <57354947+RTa-technology@users.noreply.github.com>
2020-05-06 06:50:59 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,52 +1,55 @@
-import requests
 import discord
-import xml.etree.ElementTree as ET
-import os
+import urllib.request
+import json
 import re
-import traceback
 
-token = os.environ['DISCORD_BOT_TOKEN']
 client = discord.Client()
+token = "NzA3Mjk1ODE0MjU0NTkyMDQy.XrGuoA.ICQVLi2BnLlHfbaotl-x2rSS440"
+
+citycodes = {
+    "土浦": '080020',
+    "水戸": '080010',
+    "札幌": '016010',
+    "仙台": '040010',
+    "東京": '130010',
+    "横浜": '140010',
+    "名古屋": '230010',
+    "大阪": '270000',
+    "広島": '340010',
+    "福岡": '400010',
+    "鹿児島": '460010',
+    "那覇": '471010'
+}
+
+@client.event
+async def on_ready():
+    # CHANNEL_ID = 706950934013673562 チャンネルID(AT)  
+    CHANNEL_ID = 706969662516101181#チャンネルID(AT)  
+    channel = bot.get_channel(CHANNEL_ID)  
+    await channel.send("Nervちゃん参上!")
+
 @client.event
 async def on_message(message):
-    if message.content == '!zishin':
-        er = module.newe()
-        embed = discord.Embed(title='**地震情報**', description='')
-        embed.add_field(name='発生時刻', value=er['time'], inline=True)
-        embed.add_field(name='震源地', value=er['epicenter'], inline=True)
-        embed.add_field(name='最大震度', value=er['intensity'], inline=True)
-        embed.add_field(name='マグニチュード', value=er['magnitude'], inline=True)
-        embed.add_field(name='震度1以上を観測した地域', value=er['e_1'], inline=False)
-        embed.set_image(url=er['map'])
-        await message.channel.send(embed=embed)
-def e():
-    xml_data_module = requests.get('https://www3.nhk.or.jp/sokuho/jishin/data/JishinReport.xml')
-    xml_data_module.encoding = "Shift_JIS"
-    root = ET.fromstring(xml_data_module.text)
-    for item in root.iter('item'):
-       deta_url = (item.attrib['url'])
-       break
-    deta = requests.get(deta_url)
-    deta.encoding = "Shift_JIS"
-    root = ET.fromstring(deta.text)
-    e_1 = ''
-    for Earthquake in root.iter('Earthquake'):
-        time = (Earthquake.attrib['Time'])
-        Intensity = (Earthquake.attrib['Intensity'])
-        Epicenter = (Earthquake.attrib['Epicenter'])
-        Magnitude = (Earthquake.attrib['Magnitude'])
-        Depth = (Earthquake.attrib['Depth'])
-        map_url = 'https://www3.nhk.or.jp/sokuho/jishin/'
-        count = 1
-    for Area in root.iter('Area'):
-        e_1 += '\n' + Area.attrib['Name']
-        if count == 10:
-            e_1 += '\n他'
-            break
-        count = count + 1
-    for Detail in root.iter('Detail'):
-        map = map_url + Detail.text
-        edic = {'time': time, 'epicenter': Epicenter, "intensity": Intensity, "depth": Depth, "magnitude": Magnitude, "map": map, 'e_1': e_1}
-        return edic
+  if message.author != client.user:
+
+    reg_res = re.compile(u"Bot君、(.+)の天気は？").search(message.content)
+    if reg_res:
+
+      if reg_res.group(1) in citycodes.keys():
+
+        citycode = citycodes[reg_res.group(1)]
+        resp = urllib.request.urlopen('http://weather.livedoor.com/forecast/webservice/json/v1?city=%s'%citycode).read()
+        resp = json.loads(resp.decode('utf-8'))
+
+        msg = resp['location']['city']
+        msg += "の天気は、\n"
+        for f in resp['forecasts']:
+          msg += f['dateLabel'] + "が" + f['telop'] + "\n"
+        msg += "です。"
+
+        await client.send_message(message.channel, message.author.mention + msg)
+
+      else:
+        await client.send_message(message.channel, "そこの天気はわかりません...")
 
 client.run(token)
