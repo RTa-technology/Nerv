ce0ca31156e52677f59b834cdef85c0ecdf93e5c
RTa_technology <57354947+RTa-technology@users.noreply.github.com>
2020-05-06 07:04:50 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,7 +4,9 @@ import json
 import re
 import os
 
-client = discord.Client()
+from discord.ext import commands as rta
+
+bot = rta.Bot(command_prefix='!')#, help_command=JapaneseHelpCommand()
 token = os.environ['DISCORD_BOT_TOKEN']
 
 citycodes = {
@@ -22,14 +24,31 @@ citycodes = {
     "那覇": '471010'
 }
 
-@client.event
+@bot.event
 async def on_ready():
     # CHANNEL_ID = 706950934013673562 チャンネルID(AT)  
     CHANNEL_ID = 706969662516101181#チャンネルID(AT)  
     channel = bot.get_channel(CHANNEL_ID)  
     await channel.send("Nervちゃん参上!")
 
-@client.event
+    
+    
+    
+    
+@bot.command(name="w")
+async def d(ctx, tenki: str):
+    """!d {n}d{n}の書式で入力(合計表示のみ)"""
+    resp = urllib.request.urlopen('http://weather.livedoor.com/forecast/webservice/json/v1?city=%s'%tenki).read()
+    resp = json.loads(resp.decode('utf-8'))
+    msg = resp['location']['city']
+    msg += "の天気は、\n"
+    for f in resp['forecasts']:
+        msg += f['dateLabel'] + "が" + f['telop'] + "\n"
+        msg += "です。"
+    await ctx.send(message.author.mention + msg)
+
+
+@bot.event
 async def on_message(message):
   if message.author != client.user:
 
@@ -45,9 +64,9 @@ async def on_message(message):
         msg = resp['location']['city']
         msg += "の天気は、\n"
         for f in resp['forecasts']:
-          msg += f['dateLabel'] + "が" + f['telop'] + "\n"
-        msg += "です。"
-
+            msg += f['dateLabel'] + "が" + f['telop'] + "\n"
+            msg += "です。"
+\
         await client.send_message(message.channel, message.author.mention + msg)
 
       else:
