893a00a78253e98658ded7e6361dd69217c6c89e
RTa_technology <57354947+RTa-technology@users.noreply.github.com>
2020-05-08 07:19:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -23,7 +23,7 @@ async def on_ready():
 async def on_message(message):
     a_id = message.author.id
     if a_id == nerv_id:
-        test = message.embeds[0].description
+        test = message.embeds[0].title
         await message.channel.send(test)
 
 
