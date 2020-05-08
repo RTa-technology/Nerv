f00e805cc4bf1655f35e66785c7041d3ad6f326c
RTa_technology <57354947+RTa-technology@users.noreply.github.com>
2020-05-08 09:12:30 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -19,26 +19,6 @@ async def on_ready():
     await channel.send("Nervちゃん参上ですっ!")
 
 
-@bot.event
-async def on_message(message):
-    a_id = message.author.id
-    if a_id == nerv_id:
-        title = message.embeds[0].title
-        if "地震" in title:
-            quake_intensity = message.embeds[0].fields[3].value
-            if quake_intensity == "5弱":
-                await message.channel.send("震度5弱です危険です！")
-            elif quake_intensity == "5強":
-                await message.channel.send("震度5強です危険です")
-            elif quake_intensity == "6弱":
-                await message.channel.send("震度6弱です危険です")
-            elif quake_intensity == "6強":
-                await message.channel.send("震度6強です危険です")
-            else:
-                quake_intensity = int(quake_intensity)
-                if quake_intensity < 4:
-                    await message.delete()
-
   
 
 @bot.command(name="w")
@@ -66,4 +46,30 @@ async def d(ctx, tenki: str):
         msg += "です。"
     await ctx.send(msg)
 
+
+
+
+@bot.event
+async def on_message(message):
+    a_id = message.author.id
+    if a_id == nerv_id:
+        title = message.embeds[0].title
+        if "地震" in title:
+            quake_intensity = message.embeds[0].fields[3].value
+            if quake_intensity == "5弱":
+                await message.channel.send("震度5弱です危険です！")
+            elif quake_intensity == "5強":
+                await message.channel.send("震度5強です危険です")
+            elif quake_intensity == "6弱":
+                await message.channel.send("震度6弱です危険です")
+            elif quake_intensity == "6強":
+                await message.channel.send("震度6強です危険です")
+            else:
+                quake_intensity = int(quake_intensity)
+                if quake_intensity < 4:
+                    await message.delete()
+
+    
+    
+    
 bot.run(token)
