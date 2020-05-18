77e3160f356e6b412a52361f8ab7772b700cb98a
RTa_technology <57354947+RTa-technology@users.noreply.github.com>
2020-05-19 05:33:15 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,7 +7,7 @@ from discord.ext import commands as rta
 bot = rta.Bot(command_prefix='/')#, help_command=JapaneseHelpCommand()
 token = os.environ['DISCORD_BOT_TOKEN']
 
-nerv_id = 707295814254592042
+nerv_id = 329257498668302346
 
 @bot.event
 async def on_ready():
@@ -16,36 +16,64 @@ async def on_ready():
     channel = bot.get_channel(CHANNEL_ID)  
     await channel.send("[bot]Nervちゃん参上ですっ!")
 
-
+#===============================================#
+@bot.command(name="ww")
+async def d(ctx, tenki: str):
+#    """!ww {都市名}"""
+    if tenki == "仙台":
+        resp = urllib.request.urlopen('http://weather.livedoor.com/forecast/webservice/json/v1?city=040010').read()
+    elif tenki =="東京":
+        resp = urllib.request.urlopen('http://weather.livedoor.com/forecast/webservice/json/v1?city=130010').read()
+    elif tenki =="横浜":
+        resp = urllib.request.urlopen('http://weather.livedoor.com/forecast/webservice/json/v1?city=140010').read()
+    elif tenki =="名古屋":
+        resp = urllib.request.urlopen('http://weather.livedoor.com/forecast/webservice/json/v1?city=230010').read()
+    elif tenki =="大阪":
+        resp = urllib.request.urlopen('http://weather.livedoor.com/forecast/webservice/json/v1?city=270000').read()
+    elif tenki =="岡山":
+        resp = urllib.request.urlopen('http://weather.livedoor.com/forecast/webservice/json/v1?city=330010').read()
+    elif tenki =="広島":
+        resp = urllib.request.urlopen('http://weather.livedoor.com/forecast/webservice/json/v1?city=340010').read()    
+    resp = json.loads(resp.decode('utf-8'))
+    msg = "[bot]" + resp['location']['city']
+    msg += "の天気は、\n"
+    for f in resp['forecasts']:
+        msg += f['dateLabel'] + "が" + f['telop'] + "\n"
+        msg += "です。"
+    await ctx.send(msg)
   
 
+#===============================================#
 
 
 
 
 
 @bot.event
+info_ID = 706969662516101181
+info_area_kanto = ["茨城", "千葉", "房総", "伊豆", "相模", "静岡", "埼玉", "東京", "山梨", "長野", "神奈川", "駿河", "新島", "関東", "栃木", "群馬", "三宅"]
+info_area_kinki = ["福井", "滋賀", "三重", "伊勢", "三河", "灘", "奈良", "和歌山", "大阪", "京都", "兵庫", "淡路", "播磨", "紀伊", "鳥取", "四国", "南海", "東海", "土佐", "香川", "徳島", "高知", "岡山", "瀬戸", "愛媛", "広島", "若狭"]
+info_area_exp = ["岐阜"]
+info = client.get_channel(CHANNEL_ID)
 async def on_message(message):
     a_id = message.author.id
-    if message.channel.id == 707325308331622470 or message.channel.id == 707530329535021076:
+    if message.channel.id == 708044782366097478:
         if a_id == nerv_id:
-            if not message.content.startswith("[bot]"):
-                title = message.embeds[0].title
-                if "地震" in title:
+            title = message.embeds[0].title
+            if "地震" in title:
+                area = message.embeds[0].fields[0].value
+                if info_area_exp in area or info_area_kinki in area or info_area_kanto in area:
                     quake_intensity = message.embeds[0].fields[3].value
                     if quake_intensity == "5弱":
-                        await message.channel.send("震度5弱です危険です！")
+                        await info.send("震度5弱です危険です！")
                     elif quake_intensity == "5強":
-                        await message.channel.send("震度5強です危険です")
+                        await info.send("震度5強です危険です")
                     elif quake_intensity == "6弱":
-                        await message.channel.send("震度6弱です危険です")
+                        await info.send("震度6弱です危険です")
                     elif quake_intensity == "6強":
-                        await message.channel.send("震度6強です危険です")
+                        await info.send("震度6強です危険です")
                     else:
-                        quake_intensity = int(quake_intensity)
-                        if quake_intensity < 4:
-                            await message.delete()
-
+                        await info.send("@everyone 試し")
     
     
     
